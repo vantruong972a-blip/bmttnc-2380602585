e976efd03238562973db713a718ed8f741d8e43f
vantruong972a-blip <vantruong972a@gmail.com>
2026-05-20 09:55:44 +0700

    all

--- ex02_03.py
+++
@@ -1,5 +0,0 @@
-so = int(input("Nhập một số nguyên: "))
-if so % 2 == 0:
-        print(so, "dien tich")
-else:
-        print(so, "không phải là số chẵn")
--- hello.py
+++
@@ -1,3 +0,0 @@
-print("Hello world")
-print("My name is Vu")
-print("HUTECH University")
